619847ede229fb73174e65c5794bcc7fd02b640f
Rafael Guedes <r.guedes@metocean.co.nz>
2018-02-01 11:45:54 +1300

    Make it compatible with python2

--- pint_msl/unit_registery.py
+++ pint_msl/unit_registery.py
@@ -2,7 +2,7 @@ from pint import UnitRegistry
 
 class MSLUnitRegistery(UnitRegistry):
     def __init__(self):
-        super().__init__()
+        super(MSLUnitRegistery, self).__init__()
         self.define('percent = 0.01 * count = pct')
         self.define('perthousand = 0.001 * count = ptshd')
         self.define('utc = count')
