c3e1008cb76290a08e81be1a9399e06734b6da2e
Rafael Guedes <r.guedes@metocean.co.nz>
2018-02-01 11:46:54 +1300

    Changed class name for consistency

--- pint_msl/unit_registery.py
+++ pint_msl/unit_registery.py
@@ -1,8 +1,8 @@
 from pint import UnitRegistry
 
-class MSLUnitRegistery(UnitRegistry):
+class MSLUnitRegistry(UnitRegistry):
     def __init__(self):
-        super(MSLUnitRegistery, self).__init__()
+        super(MSLUnitRegistry, self).__init__()
         self.define('percent = 0.01 * count = pct')
         self.define('perthousand = 0.001 * count = ptshd')
         self.define('utc = count')
